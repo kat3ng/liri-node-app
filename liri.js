ba82eefcfc31b65d944b79df9e30cd572a186f0d
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-11-07 22:52:34 -0600

    working logic

--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
-// UTILIZE REQUEST
+// REQUIRE REQUEST
 let request = require("request");
+
+// REQUIRE MOMENT
+const moment = require('moment');
 
 //REQUIRE FILE SYSTEMS
 const fs = require("fs");
@@ -12,72 +15,82 @@ const keys = require("./keys.js");
 // INITIALIZE SPOTIFY
 const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
-// console.log(spotify);
 
 // OMDB AND BANDS IN TOWN API'S
 let omdb = (keys.omdb);
-console.log(omdb);
+// console.log(omdb);
 let bandsintown = (keys.bandsintown);
-console.log(bandsintown);
+// console.log(bandsintown);
 
-// get the user input
+// TAKE USER COMMAND AND INPUT
 const userInput = process.argv[2];
 let userQuery = process.argv.slice(3).join(" ");
 
-// make a decision based on the command
-// switch (userInput) {
-//     case "concert-this":
-//         concertThis();
-//         break;
-//         // case "spotify-this-song":
-//         spotifyThisSong();
-//         break;
-//     case "movie-this":
-//         movieThis();
-//         break;
-//     case "do-what-it-says":
-//         doThis();
-//         break;
-//     default:
-//         console.log("I don't understand");
-//         break;
-// }
 
-// userCommand(userInput, userQuery);
+// APP LOGIC
+function userCommand(userInput, userQuery) {
+    // make a decision based on the command
+    switch (userInput) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-this":
+            doThis(userQuery);
+            break;
+        default:
+            console.log("I don't understand");
+            break;
+    }
+}
 
-// function concertThis() {
-//     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-//     console.log("CONCERT THIS");
+userCommand(userInput, userQuery);
 
-//     //request venue, location and date of event with MM/DD/YYYY formatting
-//     request(queryUrl, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//             console.log("Venue: " + JSON.parse(body).Year);
-//         }
-//     })
-// }
-// input.concertThis();
+function concertThis() {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    console.log("CONCERT THIS");
 
-// function spotifyThisSong() {
-//     console.log("SPOTIFY THIS SONG");
-// if (!userQuery.length) {
-//     userQuery = "ace of base the sing"
-// };
-//     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-//     console.log(queryUrl);
-//     console.log("MOVIE THIS");
+    //request venue, location and date of event with MM/DD/YYYY formatting
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("Venue: " + JSON.parse(body).Year);
+        }
+    })
+}
 
-//     // display artist, song name, preview link, and album
-//     // if no is provided, program will default to Ace of Base
+function spotifyThisSong() {
+    console.log(`\n- - - - -\n\nSEARCHING FOR..."${userQuery}"`);
+    //first check if unserQuery has input, if not pass in "The Sign" by Ace of Base
+    if (!userQuery) {
+        userQuery = "the sign ace of base"
+    };
 
-//     request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//         .then(function (data) {
-//             console.log(data);
-//         })
-//         .catch(function (err) {
-//             console.error('Error occurred: ' + err);
-//         });
-// }
+    spotify.search({ type: 'track', query: userQuery, limit: 3 }, function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
+
+        spotify.search({ type: 'track', query: userQuery, limit: 1 }, function (error, data) {
+            if (error) {
+                return console.log('Error occurred: ' + error);
+            }
+            //Give the for loop an array:
+            let spotifyArr = data.tracks.items;
+
+            for (i = 0; i < spotifyArr.length; i++) {
+
+                console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\nAlbum: ${data.tracks.items[i].album.name}
+               \n- - - - -`)
+            };
+        });
+    }
+    )
+}
 
 function movieThis() {
     // If no movie is given, the program will default ot 'Mr. Nobody'
@@ -92,31 +105,37 @@ function movieThis() {
 
         // If the request is successful
         if (error) {
-            return console.log("Movie not found :(" + error)
+            return console.log("Movie able to be found. Error:" + error)
         }
         else {
-            console.log(`Title: ${userMovie.Title}\n Cast: ${userMovie.Actors}\n Released: ${userMovie.Year}\n IMDb Rating: ${userMovie.imdbRating}\n Rotten Tomato Rating: ${userMovie.rottentomato}\n Country: ${userMovie.Country}\n Language: ${userMovie.Language}\n Plot: ${userMovie.Plot}\n      `)
+            console.log(`\nBA DA BOP!  That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomato Rating: ${userMovie.rottentomato}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
         };
 
     })
 };
+
 function doThis() {
-    // utilize the fs node package
-    // take the text inside of random.txt and then use it to call one of LIRI's commands
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
         }
-        var dataArr = data.split(",");
+        let dataArr = data.split(",");
         console.log(dataArr);
 
-        // it should run a "spotify-this-song" for "I Want It That Way" as follows in the text in random.txt
         dataArr.forEach(function (song) { console.log(song) });
-        // Edit the text in random.txt to test out the feature for movie - this and concert - this
-    })
-    console.log("DO THIS");
-}
+
+        // Take items from array to pass as parameters
+        // userInput = dataArr[0];
+        // userQuery = dataArr[1];
+        // It should run spotify-this for "I Want it That Way," as follows the text in random.txt
+        //Change the text in random.txt to a different command and see what happens...
+        userCommand(userInput, userQuery);
+    });
+};
+
+
+// spotifyThisSong();
 // concertThis();
-// spotifyThis();
-doThis();
-movieThis();
+// doThis();
+// movieThis();
+
