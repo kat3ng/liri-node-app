c3701460ba0ab61d6ed842ceee222fd80314fb23
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-11-07 23:52:46 -0600

    mark up edited and made uniform

--- liri.js
+++ liri.js
@@ -52,13 +52,17 @@ userCommand(userInput, userQuery);
 
 function concertThis() {
     console.log(`\n - - - - -\n\nSEARCHING FOR...${userQuery}'s next show...`);
+    // USE REQUEST AS OUR QUERY URL USING OUR USER QUERY VARIABLE AS THE PARAMETERS OF OUR SEARCH
     request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
+        // IF THERE IS NO ERROR GIVE US A 200 STATUS CODE (EVERYTHING OK!)
         if (!error && response.statusCode === 200) {
             // CAPTURE DATA AND USE JSON TO FORMAT
             let userBand = JSON.parse(body);
-
+            // PARSE DATA AND USE FOR LOOP TO ACCESS PATHS TO DATA
             if (userBand.length > 0) {
                 for (i = 0; i < 1; i++) {
+
+                    // CONSOLE DESIRED DATA USING E6 SYNTAX
                     console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}\nVenue Address: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
 
                     // MOMENT.JS TO FORMAT THE DATE MM/DD/YYYY
@@ -83,10 +87,9 @@ function spotifyThisSong() {
         if (error) {
             return console.log('Error occurred: ' + error);
         }
-        // COLLECT DATA IN AN ARRAY
+        // COLLECT SELECTED DATA IN AN ARRAY
         let spotifyArr = data.tracks.items;
 
-        // FOR LOOP TO GO THROUGH SEARCH RESULT PATHS
         for (i = 0; i < spotifyArr.length; i++) {
             console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\nAlbum: ${data.tracks.items[i].album.name}\n\n - - - - -`)
         };
@@ -95,35 +98,29 @@ function spotifyThisSong() {
 
 function movieThis() {
     console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
-    // IF USER QUERY NOT FOUND, PASS VALUE OF "MR. NOBODY"
     if (!userQuery) { userQuery = "mr nobody"; };
-
+    // REQUEST USING OMDB API
     request("http://www.omdbapi.com/?t=" + userQuery + "&apikey=86fe999c", function (error, response, body) {
-        // CAPTURE DATA AND USE JSON TO FORMAT
         let userMovie = JSON.parse(body);
-
-        // If the request is successful
-        if (error) {
-            return console.log("Movie able to be found. Error:" + error)
-        }
-        else {
+        if (!error && response.statusCode === 200) {
             console.log(`\nBA DA BOP!  That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomato Rating: ${userMovie.rottentomato}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
+        } else {
+            return console.log("Movie able to be found. Error:" + error)
         };
-
     })
 };
 
 function doThis() {
+    // UTILIZE THE BUILT IN READFILE METHOD TO ACCESS RANDOM.TXT
     fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
-        }
+        if (error) { return console.log(error); }
+        // CATCH DATA AND USE THE .SPLIT() METHOD TO SEPARATE OBJECTS WITHIN OUR NEW ARRAY
         let dataArr = data.split(",");
 
-        // Take items from random.txt array to pass as parameters
+        // TAKE OBJECTS FROM RANDOM.TXT TO PASS AS PARAMETERS
         userInput = dataArr[0];
         userQuery = dataArr[1];
-        // Runs text in random.txt
+        // CALL OUR FUNCTION WITH OUR NEW PARAMETERS...
         userCommand(userInput, userQuery);
     });
 };
