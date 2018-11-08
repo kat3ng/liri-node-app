e5baf6e2bedbea66dbe05dffabe35fb809968a53
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-11-08 00:25:47 -0600

    rotten tomatoes works

--- liri.js
+++ liri.js
@@ -102,8 +102,15 @@ function movieThis() {
     // REQUEST USING OMDB API
     request("http://www.omdbapi.com/?t=" + userQuery + "&apikey=86fe999c", function (error, response, body) {
         let userMovie = JSON.parse(body);
+
+        // BECAUSE THE ROTTEN TOMATOES RATING WAS NESTED IT WAS NECESSARY TO CAPTURE ITS VALUES IN AN ARRAY TO CREATE A PATH
+        let ratingsArr = userMovie.Ratings;
+        if (ratingsArr.length > 2) {
+            console.log("Rotten Tomatoes Rating: ", userMovie.Ratings[1].Value);
+        }
+
         if (!error && response.statusCode === 200) {
-            console.log(`\nBA DA BOP!  That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomato Rating: ${userMovie.rottentomato}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
+            console.log(`\nBA DA BOP!  That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratings[1].Value}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
         } else {
             return console.log("Movie able to be found. Error:" + error)
         };
